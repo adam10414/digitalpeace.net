a0ca59db23acc48b4e1da0d6a734aff764cdccbc
Austin Krohn <16946083+garden-r@users.noreply.github.com>
2021-06-06 18:22:08 -0500

    Adding kickstart utility; no MFA required

---
+++ utils/kicker/kickstart.py
@@ -0,0 +1,62 @@
+"""
+returns instances in plain JSON,
+ or starts the one when called with the -on argument
+"""
+
+import json
+import os
+import requests
+import argparse
+import boto3
+from botocore.exceptions import ClientError
+
+# set our global boto variables
+client = boto3.client('sts')
+ec2 = boto3.client('ec2')
+
+# ready our parser and args
+parser = argparse.ArgumentParser()
+parser.add_argument("-on", help="turns on the webserver",
+                    action="store_true") # allows for conditional evaluation
+args = parser.parse_args()
+
+
+def get_temp_creds(): # ~just MFA things~
+    creds = client.get_session_token(
+        DurationSeconds=900, # i.e. 15 minutes
+        SerialNumber='arn:aws:iam::997723378435:mfa/austin-dev', # virtual only
+        TokenCode=input('enter the 6-digit code: ')
+)
+
+def get_ec2s():
+    response = ec2.describe_instances()
+    print(response)
+
+def kickstart():
+    try: # dry run to verify permissions
+        response = ec2.start_instances(
+        InstanceIds=[
+        'i-035c5e31e024f530f'], #todo: replace hardcoded instance ids
+        DryRun=True)
+
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            raise
+
+    # success; run for real this time
+    try:
+        response = ec2.start_instances(
+        InstanceIds=[
+        'i-035c5e31e024f530f'],
+        DryRun=False)
+
+        print(response)
+    except ClientError as e:
+        print(e)
+
+# run it
+if args.on:
+    kickstart()
+
+else:
+    get_ec2s()
