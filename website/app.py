2f217e14c8184e7750a981de342b46560d329089
Waylon <68206719+adam10414@users.noreply.github.com>
2021-07-04 11:39:04 -0500

    Database set up, models declared.

--- website/app.py
+++ website/app.py
@@ -2,14 +2,39 @@
 This Flask app contains all of the routes necessary to run digitalpeace.net
 """
 
+import os
+
 from flask import Flask, render_template
 from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
 
 from .forms import NewPostSubmissionForm
 
 server = Flask(__name__, static_url_path='/static')
 server.config['SECRET_KEY'] = 'my_secret'
+server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///digitalpeaceDB.db'
+server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 csrf = CSRFProtect(server)
+
+db = SQLAlchemy(server)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), index=True)
+
+    posts = db.relationship('Post', backref=db.backref('posts', lazy=True))
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), index=True, nullable=False)
+    post_body = db.Column(db.String(), index=False, nullable=False)
+    image_file_name = db.Column(db.String(), index=True, nullable=False)
+    image_caption = db.Column(db.String(50), index=True, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
 @server.route('/')
@@ -39,8 +64,9 @@ def submit_post():
     #TODO:
     #Add logic to determine user, and post to the appropriate page.
     #If no user is logged in, then it should post the guest page.
-    #Limit number of posts by IP. (3 posts should be enough for testing.)
+    #Limit number of posts by IP if not logged in. (3 posts should be enough for testing.)
 
+    #Do stuff with the form data here:
     if new_post.validate_on_submit():
         print("Form submitted!")
         post_title = new_post.title.data
@@ -50,8 +76,38 @@ def submit_post():
         post_image_file_name = new_post.image.data.filename
         post_image = new_post.image.data
 
-        print(post_image_file_name)
-        post_image.save(f'./static/images/test/{post_image_file_name}')
+        #TODO:
+        #Find a faster way of doing this.
+
+        #Checking to see if the file name exists already, and if so, append a number to the end of the filename.
+        counter = 2
+        while os.path.exists(
+                f'./static/images/post_images/{post_image_file_name}'):
+
+            #Temporarily removing the file extension, and inserting the counter.
+            file_extension = post_image_file_name[post_image_file_name.find('.'
+                                                                            ):]
+            post_image_file_name = post_image_file_name[:post_image_file_name.
+                                                        find('.')]
+
+            post_image_file_name = post_image_file_name + str(counter)
+            counter += 1
+
+            #Adding the file extension back to the image.
+            post_image_file_name += file_extension
+
+            #TODO:
+            #Currently this counter will append numbers to the end of the file name like this:
+            #test.jpg
+            #test2.jpg
+            #test23.jpg
+            #test.234.jpg
+            #etc...
+
+            #While totally unimportant, but a nice to have:
+            #Find a way to increment the filename by 1, rather than just appending a number to the end of the filename.
+
+        post_image.save(f'./static/images/post_images/{post_image_file_name}')
 
         return render_template('submit.html',
                                new_post=new_post,
