5a08b24cac6dac4ba766ee982465666efa0e8a46
Waylon <68206719+adam10414@users.noreply.github.com>
2022-01-15 15:22:51 -0600

    Initial commit.

---
+++ digitalpace_net/config/settings/test.py
@@ -0,0 +1,29 @@
+"""
+With these settings, tests run faster.
+"""
+
+from .base import *  # noqa
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="Sfki9np4meJLqfyqWU5xVh7kM6xXPGDttfZmdmDk1KvtQmyPykE1u8IdTQYL0dZ2",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+# PASSWORDS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+# Your stuff...
+# ------------------------------------------------------------------------------
---
+++ digitalpace_net/digitalpace_net/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from digitalpace_net.users.models import User
+from digitalpace_net.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
